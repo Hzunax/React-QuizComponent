44dc84758c36b8ec0229e28d9dc9ff5c1a495ad5
Hzunax <stijn.van.asschodt@gmail.com>
2018-11-26 12:28:51 +0100

    Completed module 5 (Displaying quiz questions and handling events)

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,9 +15,13 @@ class Quiz extends Component {
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         );
+    }
+
+    showNextQuestion() {
+        this.state.quiz_position += 1;
     }
 }
 
